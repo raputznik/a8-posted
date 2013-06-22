60b52f2264ba1e7e1db493e2730ff67d5842610e
Jeremy Dahl <jeremy@palebluedot.ca>
2013-06-21 19:59:23 -0700

    Found some broken div tags, removed
    php wasn't working because of some over-commenting of HTML code. php
    posts variables now.

--- form-process.php
+++ form-process.php
@@ -1,26 +1,22 @@
 <?php 
 
-$noun = $_POST["noun"];
+$noun = $_POST["noun1"];
 $verb1 = $_POST["verb1"];
 $noun2 = $_POST["noun2"];
 $mood1 = $_POST["mood1"];
 $verb2 = $_POST["verb2"];
 $noun3 = $_POST["noun3"];
-$email = $_POST["email"]
 
 print_r($_POST);
 
-$email = "";
-
 $message = "";
 
-$message = $noun." went to the store, where he ".$verb1." his ".$noun2." and this made him ".$mood1." until he realized that he ".$verb2." his ".$noun3." forever";
+$message = $noun1." went to the store, where he ".$verb1." his ".$noun2." and this made him ".$mood1." until he realized that he ".$verb2." his ".$noun3." forever";
 
 $message = wordwrap($message, 70, "\r\n");
 
-$headers = "From: Jesus@ramsay.com";
+$headers = "From: gordon@ramsay.com";
 
 // mail(recipient, subject, message);
-mail($email, "I've got a madlib for YO!", $message, $headers);
-// echo mail("jeremy@palebluedot.ca", "I've got a madlib for YO!", $message, $headers);
+echo mail("jeremy@palebluedot.ca", "I've got a madlib for YO!", $message, $headers);
  ?>

71c5d171ece36a9ac4b7381610a2bd3793097ac6
Jeremy Dahl <jeremy@palebluedot.ca>
2013-06-21 21:59:01 -0700

    Added correct php email variable

--- form-process.php
+++ form-process.php
@@ -6,17 +6,18 @@ $noun2 = $_POST["noun2"];
 $mood1 = $_POST["mood1"];
 $verb2 = $_POST["verb2"];
 $noun3 = $_POST["noun3"];
+$email = $_POST["email"];
 
 print_r($_POST);
 
 $message = "";
 
-$message = $noun1." went to the store, where he ".$verb1." his ".$noun2." and this made him ".$mood1." until he realized that he ".$verb2." his ".$noun3." forever";
+$message = $noun1." went to the store, where he ".$verb1." his ".$noun2." and this made him ".$mood1." until he realized that he ".$verb2." his ".$noun3." was lost forever";
 
 $message = wordwrap($message, 70, "\r\n");
 
 $headers = "From: gordon@ramsay.com";
 
 // mail(recipient, subject, message);
-echo mail("jeremy@palebluedot.ca", "I've got a madlib for YO!", $message, $headers);
+echo mail($email, "I've got a madlib for YO!", $message, $headers);
  ?>
